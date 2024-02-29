b49ad92a0276bc044c09959d43986651b45ea23f
skyjo3 <skyjo3@gmail.com>
2024-02-29 15:23:50 -0800

    543 diameter of bst

---
+++ 543_diameter_BST.swift
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+// Create a new class that extends TreeNode.
+// 1. Create new variables of "score" that add the max path from the left, and the max path on the right.
+// 2. Each node's value can be the bigger value from left and right.
+
+public class NewNode: TreeNode {
+
+}
+
+class Solution {
+    
+    func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+        if let root = root {
+
+        }
+    }
+}
