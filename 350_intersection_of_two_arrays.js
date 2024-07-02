0d0850d05b221ab80b5de68dc773fe882725f075
skyjo3 <skyjo3@gmail.com>
2024-07-02 16:30:04 -0700

    350

---
+++ 350_intersection_of_two_arrays.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    var array = []
+    for (var i=0; i<nums1.length; i++) {
+        for (var j=0; j<nums2.length; j++) {
+            if (nums1[i] == nums2[j]) {
+                array.push(nums1[i])
+                break
+            }
+            console.log(nums1[i], nums2[j])
+        }
+    }
+    return array
+};
