cbdd9c9b451033c27eadf71417a5bd1556b2911d
Amy Y. Chen <skyjo3@gmail.com>
2024-07-19 20:26:39 -0700

    Update 1_two_sum.swift

--- 1_two_sum.swift
+++ 1_two_sum.swift
@@ -1,15 +1,24 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-
-        for i in 0..<nums.count - 1 {
-            for j in i..<nums.count where i != j {
-                if nums[i] + nums[j] == target {
-                    return [i, j]
-                }
+        // Solution 1: Brutal Force - O(n^2)
+        // for i in 0..<nums.count {
+        //     for j in i..<nums.count {
+        //         if nums[i]+nums[j] == target {
+        //             return [i, j]
+        //         }
+        //     }
+        // }
+        // return [0,0]
+        
+        // Solution 2: Hashmap
+        var hash : [Int:Int] = [:]
+        for i in 0..<nums.count {
+            let diff = target - nums[i]
+            if hash.keys.contains(nums[i]) {
+                return [i, hash[nums[i]]!]
             }
-            
+            hash[diff] = i
         }
-        print("Something wrong. There's no solution.")
-        return [0, target]
+        return [0,0]
     }
 }
