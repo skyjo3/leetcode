616c857c7e66345e8ed48e0fd938f2ef28c79445
Amy Y. Chen <skyjo3@gmail.com>
2024-07-19 20:29:32 -0700

    Update 3_longest_substring.swift

--- 3_longest_substring.swift
+++ 3_longest_substring.swift
@@ -1,6 +1,6 @@
 class Solution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        // Solution: Sliding Window
+        // Solution 1: Sliding Window
         if s.isEmpty {
             return 0
         }
@@ -32,5 +32,28 @@ class Solution {
         } while r_index < arr.count
 
         return maxCount
+        
+        // Solution 2: Stack
+        var set1 : Set<Character> = []
+        let arr = Array(s)
+        var sub : [Character] = []
+        var maxVal = 0
+        
+        for i in 0..<arr.count {
+            if set1.contains(arr[i]) {
+                while sub.contains(arr[i]) {
+                    if sub[0] != arr[i] {
+                        set1.remove(sub[0])
+                    }
+                    sub.removeFirst()
+                }
+                sub.append(arr[i])
+            } else {
+                set1.insert(arr[i])
+                sub.append(arr[i])
+                maxVal = max(maxVal, sub.count)
+            }
+        }
+        return maxVal
     }
 }
