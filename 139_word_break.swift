ab664cc5b1e7560c1ea37573fe40ef2e890e2bee
Amy Y. Chen <skyjo3@gmail.com>
2024-07-21 12:23:53 -0700

    Update 139_word_break.swift

--- 139_word_break.swift
+++ 139_word_break.swift
@@ -1,13 +1,26 @@
 class Solution {
     func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
-        // Solution 1: Brutal Force
-        return checkWord(s, wordDict)
+        // Solution 1: Brute Force
+        // return checkWord(s, wordDict)
 
-        // Solution 2: Two Pointer
-        var l = 0
-        var r = s.count - 1
-        var arr = Array(s)
+        // Solution 2: Dynamic Programming
+        var dp = Array(repeating: false, count: s.count+1)
+        dp[s.count] = true
 
+        for i in stride(from: s.count-1, to: -1, by: -1) {
+            for w in wordDict {
+                if i + w.count <= s.count {
+                    let subArr = Array(s)
+                    if w == String(subArr[i...i+w.count-1]) {
+                        dp[i] = dp[i + w.count]
+                    }
+                }
+                if dp[i] {
+                    break
+                }
+            }
+        }
+        return dp[0]
     }
 
     func checkWord (_ str: String, _ wrds: [String]) -> Bool {
@@ -30,7 +43,6 @@ class Solution {
                     hasRes = hasRes || checkWord(String(input), wrds)
                 } else { return true }
             }
-            
         }
         return hasRes
     }
